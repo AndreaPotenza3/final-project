3e1ff57942c9e787dabdf749034b5d15f8120cb0
AndreaPotenza <andreapoten3@gmail.com>
2025-06-08 11:43:21 +0200

    create db and entities

---
+++ src/main/java/org/exam/java/project/final_project/model/Videogame.java
@@ -0,0 +1,100 @@
+package org.exam.java.project.final_project.model;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "videogames")
+public class Videogame {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "The name cannot be null, blank or empty")
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Lob
+    @NotBlank(message = "The name cannot be null, blank or empty")
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "image")
+    private String image;
+
+    @Column(name = "category", nullable = false)
+    private String category;
+
+    @NotNull
+    @Column(name = "publication_date")
+    private LocalDate publicationDate;
+
+    @ManyToMany
+    @JoinTable(name = "platform_videogame", joinColumns = @JoinColumn(name = "videogame_id"), inverseJoinColumns = @JoinColumn(name = "platform_id"))
+    private List<Platform> platforms;
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return this.image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getCategory() {
+        return this.category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+
+    public List<Platform> getPlatforms() {
+        return this.platforms;
+    }
+
+    public void setPlatforms(List<Platform> platforms) {
+        this.platforms = platforms;
+    }
+
+
+}
