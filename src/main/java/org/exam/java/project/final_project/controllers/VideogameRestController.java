d6d770558c710dd8711d56f9258cae3e18e5682a
AndreaPotenza <andreapoten3@gmail.com>
2025-07-06 21:28:48 +0200

    fixed modal

--- src/main/java/org/exam/java/project/final_project/controllers/VideogameRestController.java
+++ src/main/java/org/exam/java/project/final_project/controllers/VideogameRestController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/videogames")
+@CrossOrigin(origins = "http://localhost:5173")
 public class VideogameRestController {
     
     @Autowired
